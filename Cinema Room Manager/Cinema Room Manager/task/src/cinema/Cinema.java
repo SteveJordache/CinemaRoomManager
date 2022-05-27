0b1a87bc991a6e8ea7e5a4524db8dab8c5805d12
stevejordache <s.iordache@gmail.com>
2022-05-27 17:08:32 +0300

    5 of 5

--- Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
+++ Cinema Room Manager/Cinema Room Manager/task/src/cinema/Cinema.java
@@ -9,6 +9,11 @@ public class Cinema {
     static boolean bigRoom;
     static int nrOfRows;
     static int nrOfSeats;
+    static long income = 0;
+    static double occupancyPercentage = 0;
+    static int numberOfPurchasedTickets = 0;
+    static int totalCapacity = 0;
+
 
     public static void printTheSeats(int nrOfRows, int nrOfSeats) {
         System.out.println("Cinema:");
@@ -32,13 +37,28 @@ public class Cinema {
         int rowNumber = scanner.nextInt();
         System.out.println("Enter a seat number in that row:");
         int seatNumber = scanner.nextInt();
-        if (bigRoom && rowNumber > nrOfRows / 2 ) {
-            System.out.println("Ticket price: $8");
+        if ("S".equals(seating[rowNumber - 1][seatNumber -1])){
+            if (bigRoom && rowNumber > nrOfRows / 2 ) {
+                System.out.println("Ticket price: $8");
+                income +=  8;
+            } else {
+                System.out.println("Ticket price: $10");
+                income += 10;
+            }
+            seating[rowNumber - 1][seatNumber -1] = "B";
+            numberOfPurchasedTickets += 1 ;
+            occupancyPercentage = (double) (numberOfPurchasedTickets * 100) / totalCapacity;
         } else {
-            System.out.println("Ticket price: $10");
+            System.out.println("That ticket has already been purchased!");
         }
-        seating[rowNumber - 1][seatNumber -1] = "B";
+    }
 
+
+    public static void statistics(String[][] seating) {
+        System.out.printf("Number of purchased tickets: %d",numberOfPurchasedTickets);
+        System.out.printf("Percentage: %.2f", occupancyPercentage);
+        System.out.printf("Current income: $%d",???);
+        System.out.printf("Total income: $%d", income);
     }
 
 
@@ -48,6 +68,7 @@ public class Cinema {
         nrOfRows = scanner.nextInt();
         System.out.println("Enter the number of seats in each row:");
         nrOfSeats = scanner.nextInt();
+        totalCapacity = nrOfRows * nrOfSeats;
         bigRoom = nrOfRows * nrOfSeats >= 60 ? true : false;
         seating = new String[nrOfRows][nrOfSeats];
         for (int i = 0; i < nrOfRows; i++) {
@@ -58,7 +79,7 @@ public class Cinema {
         int flag = Integer.MAX_VALUE;
         while (flag != 0) {
             System.out.println();
-            System.out.println("1. Show the seats\n2. Buy a ticket\n0. Exit");
+            System.out.println("1. Show the seats\n2. Buy a ticket\n3. Statistics\n0. Exit");
             flag = scanner.nextInt();
             switch (flag) {
                 case 1:
@@ -67,10 +88,12 @@ public class Cinema {
                 case 2:
                     buyTicket();
                     break;
+                case 3:
+                    statistics(seating);
+                    break;
                 case 0:
                     break;
                 default:
-                    continue;
             }
 
 
