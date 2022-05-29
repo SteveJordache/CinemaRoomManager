e9049d906d8e01cfbf355121ad530a3b9a9c1d30
stevejordache <s.iordache@gmail.com>
2022-05-29 07:00:45 +0300

    Stage 2

--- Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
+++ Simple Tic-Tac-Toe/Simple Tic-Tac-Toe/task/src/tictactoe/Main.java
@@ -1,9 +1,19 @@
 package tictactoe;
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
-        System.out.println("X O X");
-        System.out.println("O X O");
-        System.out.println("X X O");
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter cells: ");
+        String symbolString = scanner.nextLine();
+        System.out.println("---------");
+        for (int j =0; j < 9; j += 3) {
+            char poz1 = symbolString.charAt(j);
+            char poz2 = symbolString.charAt(j+1);
+            char poz3 = symbolString.charAt(j+2);
+            System.out.printf("| %s %s %s |%n", poz1, poz2, poz3);
+        }
+        System.out.println("---------");
     }
 }
